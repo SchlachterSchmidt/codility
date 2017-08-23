5f3115e6578c2f9d69855393ec2c9aecf8ab2835
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-23 23:56:12 +0100

    update on solution

--- binaryGap/Solution.java
+++ binaryGap/Solution.java
@@ -2,24 +2,19 @@ package binaryGap;
 
 public class Solution {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Solution test = new Solution();
-		int x = test.solution(6);
-	}
-
 	public int solution(int n) {
 		
 		String binRep = Integer.toString(n,2);
 		System.out.println("bin rep: " + binRep);
 		
+		// trimming trailing 0's from the end of binRep
 		int i = binRep.length() -1;
 		while (binRep.charAt(i) != '1'){
 			binRep = binRep.substring(0, i);
 			i--;
 		}
 		
+		// finding longest binary gap
 		int longestBinGap = 0;
 		int currentGap = 0;
 		for (char c : binRep.toCharArray()) {
