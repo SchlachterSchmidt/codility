2769427719a9e0457b130822d56f2784b95fb2ac
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-24 09:44:40 +0100

    in oddOccurrencesInArray, recovered Solution.java and deleted old JUnit test

---
+++ oddOccurrencesInArray/Solution.java
@@ -0,0 +1,15 @@
+package oddOccurrencesInArray;
+
+public class Solution {
+	
+
+	public int solution(int[] A) {
+		
+		int result = 0;
+		for(int i : A) 
+		    result ^= i;
+		
+		return result;
+	}
+
+}
