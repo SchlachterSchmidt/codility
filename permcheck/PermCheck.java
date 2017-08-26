3a63d30d8e20b716f8f2cfe4013bbe0ef0cbbe53
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-26 12:03:28 +0100

    codility permutation check

---
+++ permcheck/PermCheck.java
@@ -0,0 +1,20 @@
+package permcheck;
+
+import java.util.HashSet;
+
+
+public class PermCheck {
+
+	public int solution(int[] A) {
+
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < A.length; i++)
+			set.add(A[i]);
+
+		for (int i = 1; i < A.length + 1; i++) 
+			if (!set.contains(i)) return 0;
+
+		return 1;
+	}
+}
