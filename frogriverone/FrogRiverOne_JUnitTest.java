dcd5169041015033cb277a03ff3ff4a0b7dd1150
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-26 14:06:43 +0100

    codility frog river one

---
+++ frogriverone/FrogRiverOne_JUnitTest.java
@@ -0,0 +1,19 @@
+package frogriverone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FrogRiverOne_JUnitTest {
+
+	private FrogRiverOne tester = new FrogRiverOne();
+	
+	@Test
+	public void baseCase() {
+		
+		int x = 5;
+		int[] A = {1, 3, 1, 4, 2, 3, 5, 4};
+		assertEquals(6, tester.solution(x, A));
+	}
+
+}
