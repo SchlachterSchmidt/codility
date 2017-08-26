a7439a3fb3650fc96b408cc58a9fc1cdfdd2ca7e
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-26 09:02:12 +0100

    codility find missing integer

---
+++ missinginteger/MissingInteger.java
@@ -0,0 +1,34 @@
+package missinginteger;
+
+import java.util.HashSet;
+
+public class MissingInteger {
+	
+	/*
+	 find the smallest positive integer that 
+	 does not occur in a given sequence.
+	 */
+
+	public int solution(int[] A) {
+
+		int max = A[0];
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > 0) {
+				set.add(A[i]);
+				if (max < A[i]) {
+					max = A[i];
+				}
+			}
+		}
+
+		for (int i = 1; i < max + 2; i++) {
+			if (!set.contains(i)) {
+				return i;
+			}
+		}
+
+		return 1;
+	}
+}
---
+++ missinginteger/MissingInteger_JUnitTest.java
@@ -0,0 +1,37 @@
+package missinginteger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MissingInteger_JUnitTest {
+
+	MissingInteger tester = new MissingInteger();
+
+	@Test
+	public void baseCasetOne() {
+		int[] A = { 1, 3, 6, 4, 1, 2 };
+		assertEquals(5, tester.solution(A));
+	}
+
+	@Test
+	public void baseCasetTwo() {
+		int[] A = { 1, 2, 3 };
+		assertEquals(4, tester.solution(A));
+	}
+
+	@Test
+	public void baseCasetThree() {
+		int[] A = { -1, -3 };
+		assertEquals(1, tester.solution(A));
+	}
+	
+
+	@Test
+	public void caseAllNeg() {
+		int[] A = { -4, -5, -3};
+		assertEquals(1, tester.solution(A));
+	}
+
+
+}
