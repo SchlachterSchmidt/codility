aa5d9a7801122be192b420fbb34c73899e3a53a5
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-08-27 13:14:27 +0100

    fixed indentation on test cases

--- countdivs/CountDivs_JUnitTest.java
+++ countdivs/CountDivs_JUnitTest.java
@@ -5,46 +5,46 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CountDivs_JUnitTest {
-	
+
 	private CountDivs tester = new CountDivs();
-	
+
 	@Test
 	public void simpleTestDivByOne() {
-		int a = 10,
-			b = 20,
-			k = 1;
+		int a = 10;
+		int b = 20;
+		int k = 1;
 		assertEquals(11, tester.solution(a, b, k));
 	}
 
 	@Test
 	public void simpleTestDivByTwo() {
-		int a = 10,
-			b = 20,
-			k = 2;
+		int a = 10;
+		int b = 20;
+		int k = 2;
 		assertEquals(6, tester.solution(a, b, k));
 	}
-	
+
 	@Test
 	public void baseCase() {
-		int a = 6,
-			b = 11,
-			k = 2;
+		int a = 6;
+		int b = 11;
+		int k = 2;
 		assertEquals(3, tester.solution(a, b, k));
 	}
-	
+
 	@Test
 	public void largeRangeSmallK() {
-		int a = 1,
-			b = Integer.MAX_VALUE,
-			k = 10;
+		int a = 1;
+		int b = Integer.MAX_VALUE;
+		int k = 10;
 		assertEquals(Integer.MAX_VALUE / 10, tester.solution(a, b, k));
 	}
-	
+
 	@Test
 	public void largeRangeLargeK() {
-		int a = 1,
-			b = Integer.MAX_VALUE,
-			k = Integer.MAX_VALUE;
+		int a = 1;
+		int b = Integer.MAX_VALUE;
+		int k = Integer.MAX_VALUE;
 		assertEquals(1, tester.solution(a, b, k));
 	}
 
--- permmissingelement/PermMissingElement_JUnitTest.java
+++ permmissingelement/PermMissingElement_JUnitTest.java
@@ -8,25 +8,21 @@ public class PermMissingElement_JUnitTest {
 	
 	private Solution tester = new Solution();
 	
-	private int[] A = {2};
-	private int[] B = {1,2,4};
-	private int[] C = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16 ,17, 18 ,19};
-
 	@Test
 	public void caseOneElement() {
-		
+		int[] A = {2};
 		assertEquals("given B = {2}, returns 1", 1, tester.solution(A));
 	}
 	
 	@Test
 	public void caseGeneralCase() {
-		
+		int[] B = {1, 2, 4};
 		assertEquals("given A = {1, 2, 4}, returns 3", 3, tester.solution(B));
 	}
 	
 	@Test
 	public void caseLargeInput() {
-		
+		int[] C = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16 ,17, 18 ,19};
 		assertEquals("given A = {1 ... 10, 12 ... 19}, returns 11", 11, tester.solution(C));
 	}	
 }
