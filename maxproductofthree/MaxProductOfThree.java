15bcc00d5057946196b3aec2556c2302676e7c21
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-09-01 14:45:39 +0100

    codility max product of three

---
+++ maxproductofthree/MaxProductOfThree.java
@@ -0,0 +1,26 @@
+package maxproductofthree;
+
+import java.util.Arrays;
+
+public class MaxProductOfThree {
+	
+	/*
+	 * Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
+	 * 
+	 * https://codility.com/demo/results/trainingWMZZJZ-CGW/
+	 */
+	
+	public int solution(int[] A) {
+		
+		int n = A.length;
+		Arrays.sort(A);
+		
+		long p = A[n - 1];
+		long q = A[n - 2];
+		long r = A[n - 3];
+		long i = A[0];
+		long j = A[1];
+
+		return (int)Math.max((p * q * r), (p * i * j));
+	}
+}
