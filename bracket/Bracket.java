3941eabe404177c09d7c93ff54748d34c7081b32
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-09-01 11:54:34 +0100

    codility bracket

---
+++ bracket/Bracket.java
@@ -0,0 +1,43 @@
+package bracket;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Bracket {
+
+	/*
+	 * Determine whether a given string of parentheses is properly nested.
+	 * 
+	 * 
+	 */
+
+	public int solution(String S) {
+
+		char[] charArr = S.toCharArray();
+		Stack<Character> charStack = new Stack<>();
+		Map<Character, Character> opposites = new HashMap<>();
+
+		opposites.put('}', '{');
+		opposites.put(']', '[');
+		opposites.put(')', '(');
+
+		for (char c : charArr) {
+
+			if (c == '[' || c == '(' || c == '{') {
+				charStack.push(c);
+			}
+
+			else {
+				if (!charStack.isEmpty()) {
+					char prev = charStack.pop();
+					if (prev != opposites.get(c)) {
+						return 0;
+					}
+				} else
+					return 0;
+			}
+		}
+		return (charStack.isEmpty() ? 1 : 0);
+	}
+}
